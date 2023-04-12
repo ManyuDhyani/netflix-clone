30f37b2398c05e22954fa4a09fb30498644130d5
ManyuDhyani <manyudhyani@gmail.com>
2023-04-12 12:04:47 -0400

    API Integration and Implementing Banner & rows.

--- src/Banner.js
+++ src/Banner.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './css/Banner.css'
+import axios from "./axios"
+import requests from './Requests';
 
 function Banner() {
+
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  const truncate = (string, n) => {
+    return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+  }
   return (
     <header className='banner' style={{
-      backgroundImage: `url("https://res.cloudinary.com/practicaldev/image/fetch/s--THrf5Yjw--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/n6brz4p7iq7j1mulo1nv.jpg")`,
+      backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
       backgroundPosition: "center center",
       backgroundSize: "cover"
     }}>
 
     <div className='banner__contents'>
-      <h1 className="banner__title">Movie Name</h1>
+      <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
       <div className="banner__buttons">
         <button className='banner__button'>Play</button>
         <button className='banner__button'>My List</button>
       </div>
       <h1 className="banner__description">
-        This is a test description
+        {truncate(movie?.overview, 150)}
       </h1>
     </div>
 
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,9 @@ import React from 'react'
 import "./css/HomeScreen.css"
 import Navbar from './Navbar'
 import Banner from './Banner'
+import requests from './Requests'
+import Row from './Row'
+
 function HomeScreen() {
   return (
     <div className='homeScreen'>
@@ -10,7 +13,14 @@ function HomeScreen() {
         {/* Banner */}
         <Banner />
         {/* Row */}
-        
+        <Row title="NETFLIX ORIGINALS" fetchUrl = {requests.fetchNetflixOriginals} isLargeRow />
+        <Row title="Trending Now" fetchUrl = {requests.fetchTreanding} isLargeRow />
+        <Row title="Top Rated" fetchUrl = {requests.fetchTopRated} isLargeRow />
+        <Row title="Action Movies" fetchUrl = {requests.fetchActionMovies} isLargeRow />
+        <Row title="Comedy Movies" fetchUrl = {requests.fetchComedyMovies} isLargeRow />
+        <Row title="Horror Movies" fetchUrl = {requests.fetchHorrorMovies} isLargeRow />
+        <Row title="Romance Movies" fetchUrl = {requests.fetchRomanceMovies} isLargeRow />
+        <Row title="Documentries" fetchUrl = {requests.fetchDocumentariesMovies} isLargeRow />
     </div>
   )
 }
